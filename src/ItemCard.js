41321c3278f1cf5e767980b19f2bce22906524fa
bochaco <bochaco@localhost.localdomain>
2016-11-08 18:18:04 -0300

    Enchancing the grid of cards.

--- src/ItemCard.js
+++ src/ItemCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Card, Image, Container } from 'semantic-ui-react';
+import { Button, Card, Grid, Image, Container } from 'semantic-ui-react';
 import icon_cc from './img/credit_card.jpg';
 import icon_pwd from './img/password.jpg';
 import icon_qr from './img/qr_code.png';
@@ -8,42 +8,45 @@ import icon_unknown from './img/unknown.png';
 
 export class ItemCard extends React.Component {
   render() {
-    var img, meta;
-    if (this.props.item.type == 0) {
+    var img, meta, color;
+    if (this.props.item.type === 0) {
       img = <Image floated='left' size='tiny' src={icon_cc} />
       meta = "Credit Card";
-    } else if (this.props.item.type == 1) {
+      color = "blue";
+    } else if (this.props.item.type === 1) {
       img = <Image floated='left' size='tiny' src={icon_pwd} />
       meta = "Password";
-    } else if (this.props.item.type == 2) {
+      color = "red";
+    } else if (this.props.item.type === 2) {
       img = <Image floated='left' size='tiny' src={icon_qr} />
       meta = "Priv/Pub Key";
-    } else if (this.props.item.type == 3) {
+      color = "yellow";
+    } else if (this.props.item.type === 3) {
       img = <Image floated='left' size='tiny' src={icon_2fa} />
       meta = "2FA Codes";
+      color = "violet";
     } else {
       img = <Image floated='left' size='tiny' src={icon_unknown} />
       meta = "";
+      color = "grey";
     }
 
     return (
-      <Card>
+      <Card itemsPerRow={3} color={color}>
         <Card.Content href='#link'>
-          {img}
-          <Card.Header>
-            {this.props.item.label}
-          </Card.Header>
           <Card.Meta>
-            <br />
+            {img}
             {meta}
           </Card.Meta>
+          <Card.Header>
+            <br/>
+            {this.props.item.label}
+          </Card.Header>
         </Card.Content>
         <Card.Content extra>
           <Container textAlign='right'>
-            <div>
-              <Button inverted circular icon='edit' color='blue'/>
-              <Button inverted circular icon='trash outline' color='blue'/>
-            </div>
+              <Button inverted circular icon='edit' color={color} />
+              <Button inverted circular icon='trash outline' color={color} />
           </Container>
         </Card.Content>
       </Card>
--- src/MainGrid.js
+++ src/MainGrid.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react'
+import { Card, Container } from 'semantic-ui-react'
 import { ItemCard } from './ItemCard.js';
 
 
 export default class MainGrid extends React.Component {
   render() {
     var cards = this.props.data.map((item, index) => (
-      <ItemCard item={item} />
+        <ItemCard item={item} />
     ));
-    return <Card.Group>{cards}</Card.Group>
+
+    return (
+      <Container>
+        <Card.Group>
+          {cards}
+        </Card.Group>
+      </Container>
+    );
   }
 }
